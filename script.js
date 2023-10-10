bc97ab8cedd4de334333031b7dca9aee17276b4a
Aman Velpula <velpmail@gmail.com>
2023-10-10 19:13:58 -0400

    3 boxes randomize

--- script.js
+++ script.js
@@ -4,17 +4,72 @@ const randomizeColor = document.querySelector('.btnBoom')
 
 const colorCard = document.querySelector('.box-one')
 
+
+
 function getRandomGradient() {
     const randomColor1 = '#' + Math.floor(Math.random()*16777215).toString(16);
     const randomColor2 = '#' + Math.floor(Math.random()*16777215).toString(16);
     return `linear-gradient(90deg, ${randomColor1}, ${randomColor2})`;
 }
 
+function getRandomGradientTwo() {
 
-randomizeColor.addEventListener("click", () => {
+    const randomColor1 = '#' + Math.floor(Math.random()*16777215).toString(16);
+    const randomColor2 = '#' + Math.floor(Math.random()*16777215).toString(16);
+    const randomColor3 = '#' + Math.floor(Math.random()*16777215).toString(16);
+    return `linear-gradient(190deg, ${randomColor1}, ${randomColor2}, ${randomColor3})`;
+
+}
+
+
+function getRandomGradientThree() {
+
+    const randomColor1 = '#' + Math.floor(Math.random()*16777215).toString(16);
+    const randomColor2 = '#' + Math.floor(Math.random()*16777215).toString(16);
+    const randomColor3 = '#' + Math.floor(Math.random()*16777215).toString(16);
+    const randomColor4 = '#' + Math.floor(Math.random()*16777215).toString(16);
+
+    return `linear-gradient(20deg, ${randomColor1}, ${randomColor2}, ${randomColor3}, ${randomColor4})`;
+    
+
+}
+
+
+document.getElementById("randomBtnOne").addEventListener("click", () => {
     const boxOne = document.querySelector(".box-one");
     boxOne.style.background = getRandomGradient();
 })
+
+
+document.getElementById("randomBtnTwo").addEventListener("click", () => {
+    const boxTwo = document.querySelector(".box-two");
+    boxTwo.style.background = getRandomGradientTwo();
+})
+
+
+document.getElementById("randomBtnThree").addEventListener("click", () => {
+    const boxThree = document.querySelector(".box-three");
+    boxThree.style.background = getRandomGradientThree();
+})
+
+
+document.getElementById("randomBtnOne").addEventListener("click", () => {
+    const boxOne = document.querySelector(".box-one");
+    boxOne.style.background = getRandomGradient();
+})
+
+
+document.getElementById("randomBtnOne").addEventListener("click", () => {
+    const boxOne = document.querySelector(".box-one");
+    boxOne.style.background = getRandomGradient();
+})
+
+
+document.getElementById("randomBtnOne").addEventListener("click", () => {
+    const boxOne = document.querySelector(".box-one");
+    boxOne.style.background = getRandomGradient();
+})
+
 
 
 
