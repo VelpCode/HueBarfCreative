22808f1dec487ed07a5db187ffe59023b3031e66
Aman Velpula <velpmail@gmail.com>
2023-10-10 18:21:03 -0400

    adding more styling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container')
+
 const cardAmount = 3;
 let bool = true;
 
@@ -15,81 +16,3 @@ function rndColor() {
 
 }
 
-container.innerHTML = `
-<nav class = "sidebar">
-        <header>
-            <img class = 'tdot' src = 'images/3dot.png'>
-            <div class = "text header-text">
-                <span class = "name">Hue Barf.</span>
-            </div>
-        </header>
-
-        <div class = "menu-bar">
-            <div class="menu">
-                <ul class ="menu-links">
-                    <li class = "nav-link">
-                        <a href = "#">
-                            <i class="fa-solid fa-house"></i>
-                            <span class = "text nav-text">Dashboard</span>
-                        </a>
-                    </li>
-                    <li class = "nav-link">
-                        <a href = "#">
-                            <i class="fa-brands fa-app-store-ios"></i>
-                            <span class = "text nav-text">Other Apps</span>
-                        </a>
-                    </li>
-                    <li class = "nav-link">
-                        <a href = "#">
-                            <i class="fa-solid fa-street-view"></i>
-                            <span class = "text nav-text">Overview</span>
-                        </a>
-                    </li>
-                    <li class = "nav-link">
-                        <a href = "#">
-                            <i class="fa-solid fa-address-card"></i>
-                            <span class = "text nav-text">About</span>
-                        </a>
-                    </li>
-                    <li class = "nav-link">
-                        <a href = "#">
-                            <i class="fa-solid fa-address-book"></i>
-                            <span class = "text nav-text">Contact Us</span>
-                        </a>
-                    </li>
-                    <li class = "nav-link">
-                        <a href = "#">
-                            <i class="fa-solid fa-hippo"></i>
-                            <span class = "text nav-text">DivSplash</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-    </nav>
-
-    <div class="card">
-        <div class="header">
-            <h1 class = 'title'>Hue Barf.</h1>
-            <h3 class = 'heading'>A gradient collection designed for you.</h3>
-        </div>
-        <div class = "hero">
-            <img class = "barfie" src = "images/barfue.png">
-        </div>
-        <div class = "divbox">
-            <div class ="boxcontainer">
-                <div class = "box-one">
-                        <p></p>
-                </div>
-                <div class = "box-two">
-                    <p></p>
-                </div>
-                <div class = "box-three">
-                    <p></p>
-                </div>
-            </div>
-        </div>
-
-    </div>
-`
