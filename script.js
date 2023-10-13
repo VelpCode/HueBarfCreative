3f67cec00cb6ea62ce4c8ef8668b625f3ea1f0c6
Aman Velpula <velpmail@gmail.com>
2023-10-13 16:41:42 -0400

    dude bro man

--- script.js
+++ script.js
@@ -74,19 +74,3 @@ document.getElementById("randomBtnSix").addEventListener("click", () => {
 
 
 
-const cardAmount = 3;
-let bool = true;
-
-let color1Arr = [];
-let color2Arr = [];
-
-function rndColor() {
-    const x = Math.round(0xfffff * Math.random()).toString(16);
-    const y = 6 - x.length;
-    const z = "000000";
-    const z1 = z.substring(0,y);
-    const randomColor = "#" + z1 + x;
-    return randomColor;
-
-}
-
