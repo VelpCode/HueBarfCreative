60c4e76e9aeb477ddf6d6b8943c2fe0e15e1e358
Aman Velpula <velpmail@gmail.com>
2023-10-15 19:28:23 -0400

    Updating color gradients

--- script.js
+++ script.js
@@ -44,10 +44,8 @@ function getRandomGradientThree() {
     const randomColor1 = '#' + Math.floor(Math.random()*16777215).toString(16);
     const randomColor2 = '#' + Math.floor(Math.random()*16777215).toString(16);
     const randomColor3 = '#' + Math.floor(Math.random()*12311243).toString(16);
-
     return `linear-gradient(20deg, ${randomColor1}, ${randomColor2}, ${randomColor3})`;
     
-
 }
 
 
